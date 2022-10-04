7581e61f2a7d9d14147e4c99495ee45ad2aff305
kitagawa <kitarira_1@icloud.com>
2022-10-04 20:51:53 +0900

    11-4まで

--- private_diary/private_diary/settings_common.py
+++ private_diary/private_diary/settings_common.py
@@ -169,3 +169,6 @@ ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
 
 # ログアウトリンクのクリック一発でログアウトする設定
 ACCOUNT_LOGOUT_ON_GET = True
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media'
--- private_diary/private_diary/settings_dev.py
+++ private_diary/private_diary/settings_dev.py
@@ -6,3 +6,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media'
